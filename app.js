af1bc7c02f1584ed4677a6bf3f45698bb5a25f6c
Alex Read <alexread730@gmail.com>
2017-07-29 17:57:01 -0600

    SPA

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-  $('.portfolio').hide();
-  $('.jumbotron').hide();
-  $('.jumbotron').fadeIn(1500);
-  $('.about').hide();
-  $('.about').fadeIn(1500);
-  $('.portfolio-button').click(() => {
-    $('.jumbotron').fadeOut(1500, () => {
-      $('.portfolio').fadeIn(1500);
-    });
-  });
 
   $('.article-img').hover(function() {
     $(this).css('opacity', '.8');
