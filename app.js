a07eaaea1abf9007b84e06f44661a6ca74453597
Alex Read <alexread730@gmail.com>
2017-08-30 10:10:36 -0600

    added contact info

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
     $(this).css('opacity', '1');
   });
 
+  $('.contact-info').hide();
+
+  $('.contact').click(event => {
+    $('.contact').fadeOut(500, event => {
+      $('.contact-info').fadeIn(500);
+    });
+
+  });
+
+    $('.contact-info').click(event => {
+      $('.contact-info').fadeOut(500, event => {
+        $('.contact').fadeIn(500);
+      });
+
+    });
+
 
 
 })
